aea00c45fbf9ea9a46f5723ff59a87838905f7f6
Heeju Suh <62493878+Miheeju@users.noreply.github.com>
2023-09-26 11:30:20 -0700

    Update ch15_1_pdfparanoia.py
    1. Removed unneeded '_encrypted' from file extension. Encrypted files saved as '.pdf', not '.pdf_encrypted'
    
    2. Added part 2: Find all encrypted PDFs in a folder + subfolders and create decrypted copies.

--- ch15_1_pdfparanoia.py
+++ ch15_1_pdfparanoia.py
@@ -22,8 +22,35 @@ for path, subfolders, files in os.walk(folder_location):
         # Set password
         pdf_writer.encrypt('dayoldcroissant')
         # Create result PDF shell
-        print(target_location + i + '_encrypted', 'wb')
-        result_pdf = open(target_location + i + '_encrypted', 'wb')
+        # print(target_location + i, 'wb')  # Optional display
+        result_pdf = open(target_location + i, 'wb')
+        # Write to it
+        pdf_writer.write(result_pdf)
+        # Close it
+        result_pdf.close()
+
+'''
+Part 2 Find all encrypted PDFs in a folder (and its subfolders) and creates a decrypted copy of the PDF using a provided password.
+If the password is incorrect, the program should print a message to the user and continue to the next PDF.
+'''
+
+folder_location = target_location
+target_location = 'C:/Users/.../Automate the Boring Stuff 2e/Decrypted Stack/' # Use your local file path
+
+for path, subfolders, files in os.walk(folder_location):
+    for i in files:
+        pdf_reader = PyPDF2.PdfFileReader(open(path + '/' + i, 'rb'))
+        # Start up PDF Writer Object
+        pdf_writer = PyPDF2.PdfFileWriter()
+        # Decrypt with password before copying
+        if pdf_reader.isEncrypted:
+            pdf_reader.decrypt('dayoldcroissant')
+        # Copy all reader pages to writer object using .addPage
+        for page_num in range(pdf_reader.numPages):
+            pdf_writer.addPage(pdf_reader.getPage(page_num))
+        # Create result PDF shell
+        print(target_location + i, 'wb') # Optional display
+        result_pdf = open(target_location + i, 'wb')
         # Write to it
         pdf_writer.write(result_pdf)
         # Close it
